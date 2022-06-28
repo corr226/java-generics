8a3e175ce80bcfc4733d5cbeb8f3faa6d5f1ec66
Chris J. Orr <CJOrr@bclc.com>
2022-06-27 17:30:46 -0700

    Hello.java

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -1,5 +1,7 @@
 package hello;
 
+/* adding comments here for the commit */
+
 public class Hello {
   public static void main(String[] args) {
     ((Runnable)()-> System.out.println("Hello World!")).run();
